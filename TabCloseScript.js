8cd75b89e01aba7b392921c45f2e0d59a6986001
Himanshu Taneja <himanshu@htaneja.in>
2017-03-02 21:00:59 -0600

    changing left to right

--- TabCloseScript.js
+++ TabCloseScript.js
@@ -1,23 +1,19 @@
-chrome.browserAction.onClicked.addListener(function(tab) {  
+chrome.browserAction.onClicked.addListener(function(tab) {
    closeLeftTabs();
 });
 
-
 function closeLeftTabs() {
-
   performActionOnCurrentTab(function(activeTab){
      if(activeTab) {
         chrome.tabs.query({currentWindow : true,pinned: false},function(tabs) {
              for (var i = 0; i < tabs.length; i++) {
-                 if (tabs[i].index < activeTab.index){
+                 if (tabs[i].index > activeTab.index){
                      chrome.tabs.remove(tabs[i].id, null);
                  }
              }
          });
      }
  });
-
-
 }
 
 function performActionOnCurrentTab(tabCallback) {
